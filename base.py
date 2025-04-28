d8589382518cea4f40abd0b64aad2fb18cceca3c
Nila <nila.ivonina@gmail.com>
2025-04-28 21:50:36 +0300

    кнопки и некоторые недочеты

--- base.py
+++ base.py
@@ -1,6 +1,8 @@
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, InputMediaPhoto
-from telegram.ext import CommandHandler, Application
+from telegram.ext import CommandHandler, Application, MessageHandler, filters
 from config import BOT_TOKEN
+
+selected_template = ''  # сюда будет записываться номер выбранного шаблона
 
 
 # Старт
@@ -14,11 +16,11 @@ async def start(update, context):
 # Функция создания новой презентации
 async def new_presentation(update, context):
     # клавиатура1
-    reply_keyboard1 = [['№1'], ['№2'], ['№3'], ['№4'], ['№5'], ['№6'], ['№7'], ['№8'], ['№9'], ['№10'], ['ещё']]
+    reply_keyboard1 = [['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', 'ещё шаблоны']]
     markup1 = ReplyKeyboardMarkup(reply_keyboard1, one_time_keyboard=True, resize_keyboard=True)
 
     await update.message.reply_text(
-        "Для начала выбери шаблон по которому я буду делать презентацию:", reply_markup=markup1)
+        "Для начала выберите шаблон по которому я буду делать презентацию:", reply_markup=markup1)
 
     IMAGE_LINKS1 = [  # Ссылки на изображения чтобы не скачивать изображеня
         'https://easy-exam.ru/static/main_page/image/tasks/880.png',
@@ -37,28 +39,37 @@ async def new_presentation(update, context):
     media_group = [InputMediaPhoto(link) for link in IMAGE_LINKS1]
     await update.message.reply_media_group(media=media_group)  # Высылаются изображения - шаблоны лдя презентаций
 
-    #
-    # text = update.message.text
-    # if text == 'ещё':
-    #     print('klk;')
+
+async def more_templates(update, context):
+    template_response = update.message.text
+    if template_response == 'ещё шаблоны':
+        # клавиатура2
+        reply_keyboard2 = [['11', '12', '13', '14'], ['15', '16', '17', '18'], ['19', '20', 'назад']]
+        markup2 = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=True, resize_keyboard=True)
+
+        await update.message.reply_text(
+            'Может тут есть нужный вам шаблон?🤔', reply_markup=markup2)
+
+        IMAGE_LINKS2 = [  # Ссылки на изображения чтобы не скачивать изображеня
+            'https://easy-exam.ru/static/main_page/image/tasks/889.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/890.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/891.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/892.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/893.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/894.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/896.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/897.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/898.png',
+            'https://easy-exam.ru/static/main_page/image/tasks/899.png'
+
+        ]
+        media_group = [InputMediaPhoto(link) for link in IMAGE_LINKS2]
+        await update.message.reply_media_group(media=media_group)  # Высылаются изображения - шаблоны лдя презентаций
+    elif template_response.isdigit() and 1 <= int(template_response) <= 20:
 
 
-async def more(update, context):
-    IMAGE_LINKS2 = [  # Ссылки на изображения чтобы не скачивать изображеня
-        'https://easy-exam.ru/static/main_page/image/tasks/889.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/890.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/891.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/892.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/893.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/894.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/896.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/897.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/898.png',
-        'https://easy-exam.ru/static/main_page/image/tasks/899.png'
-
-    ]
-    media_group = [InputMediaPhoto(link) for link in IMAGE_LINKS2]
-    await update.message.reply_media_group(media=media_group)  # Высылаются изображения - шаблоны лдя презентаций
+    else:
+        await update.message.reply_text("ошибка, нажмите на кнопку")
 
 
 # Функция помощи
@@ -84,7 +95,7 @@ def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("new_presentation", new_presentation))
-    application.add_handler(CommandHandler("more", more))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, more_templates))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("close", close_keyboard))
     print("Бот запущен")
